1d0eddf2dccb493f7fae0df14de14b0a495bc8f8
Mohamed Hashim Youssef <57812918+MohamedHashimYoussef@users.noreply.github.com>
2020-04-02 18:29:30 +0200

    integration commit

---
+++ FinalGP/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /.svg$/,
+                loader: 'svg-inline-loader'
+            }
+        ]
+    },
+
+}
