7b67889d247cab663b12183f04f2d2647a79cbd7
Ahmed gaafer <ahmedgaafer@yahoo.com>
2020-05-15 03:52:19 +0200

    Updates APIs

---
+++ FinalGP/account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from FinalGP import settings
+from .views import *
+from django.conf.urls.static import static
+urlpatterns = [
+    path('list-doctors/' , ListDoctors.as_view()),
+    path('list-doctor-patients/<int:id>/' , ListPatientsofDoctor.as_view()),
+    path('add-doctor-patient/' , AddPatientToDoctor.as_view()) ,
+    path('doctor-of-patient/<int:id>/' , DoctorOfPatient.as_view()) , 
+    path('list-patient-cases/<int:id>/' , PatientCases.as_view()) , 
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- FinalGP/account/views.py
+++ FinalGP/account/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+from .auth.permissions import *
+from .models import *
 # Create your views here.
+
+
+class ListDoctors(APIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self , request , *args , **kwargs):
+        query  = Doctor.objects.all().values()
+        data   = []
+        for i in query:
+            data.append(i)
+        return Response(data , status=200)
+    
+    
+class ListPatientsofDoctor(APIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self , request , *args , **kwargs):
+        Id = kwargs['id']
+        query  = Patient.objects.filter(relate_doctor=Id).values()
+        data   = []
+        for i in query:
+            data.append(i)
+            
+        return Response(data , status=200)
+    
+class AddPatientToDoctor(APIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def post(self , request , *args , **kwargs):
+        data  = request.data
+        Id_doctor = data.get('id_doctor')
+        email_patient = data.get('email_patient')
+        query_patient = Patient.objects.filter(Email=email_patient).first()
+        quey_doctor   = Doctor.objects.filter(id=Id_doctor).first()
+        query_patient.relate_doctor = quey_doctor
+        query_patient.save()
+        return Response({'Process' : 'done'} , status=200)
+    
+    
+class DoctorOfPatient(APIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self , request , *args , **kwargs):
+        id_patient = kwargs['id']
+        query = Patient.objects.filter(id=id_patient).first()
+        doctor_id = query.relate_doctor.id
+        return Response({'Id' : doctor_id} , status=200)
+    
+    
+class PatientCases(APIView):
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self , request , *args , **kwargs):
+        Id   = kwargs['id']
+        query  = Case.objects.filter(relate_patient=Id).values()
+        data  = []
+        for i in query:
+            data.append(i)
+            
+        return Response(data , status=200)
+        
